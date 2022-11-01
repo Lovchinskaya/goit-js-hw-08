868486e9aa6279744a1174847e3316dde1e60e97
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-11-01 13:16:55 +0200

    HW 8 3commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import {galleryItems} from "./gallery-items.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryImages(galleryItems);  
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onClickImage);
+
+
+
+function onClickImage(event) {
+    event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src="${event.target.dataset.source}" width="800" height="600">
+     </div>`,
+    {
+      onShow() {
+        document.addEventListener('keydown', closeModalImg)
+      },
+      onClose() {
+        document.removeEventListener('keydown', closeModalImg)
+      },
+    },
+  )
+  instance.show();
+
+  function closeModalImg(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+  }
+}
+}
+
