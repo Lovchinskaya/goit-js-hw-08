e7960f6c722c78a35c40a8984f2566b30deecdf0
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-11-03 12:09:00 +0200

    HW 8 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+ import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+
+form.addEventListener('submit', saveData);
+
+
+function saveData(evt) {
+    evt.preventDefault();
+    const {
+    elements: { email, message }
+  } = evt.currentTarget;
+
+    const data = {
+        email: email.value,
+        message: message.value,
+    } 
+    if (data === null) {
+        localStorage.clear('data', JSON.stringify(data));
+    } else {
+          localStorage.setItem('data', JSON.stringify(data));
+localStorage.getItem('timeupdate', throttle(saveData, 500));
+
+    }
+        console.log(data);
+  form.reset();
+}
 
